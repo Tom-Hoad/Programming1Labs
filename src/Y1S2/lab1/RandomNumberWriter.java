c9a01f6686dd19d45157f9a56722909a3cef27e6
Tom-Hoad <tom.hoad03@gmail.com>
2020-03-03 16:09:59 +0000

    Added library for comp1206

--- src/Y1S2/lab1/RandomNumberWriter.java
+++ src/Y1S2/lab1/RandomNumberWriter.java
@@ -1,4 +1,23 @@
 package Y1S2.lab1;
 
+import java.util.Random;
+
+import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io.RandomIO;
+
 public class RandomNumberWriter implements RandomIO {
+    private Random random;
+
+    public RandomNumberWriter(long seed) {
+        this.random = new Random(seed);
+    }
+
+    public void writeRandomByte(String filename) {
+
+    }
+
+    public void writeRandomChars(String filename) {
+
+    }
+
+
 }
