6ee19ad47c3ac50c8ed695302637badb2dc4824f
Tom-Hoad <tom.hoad03@gmail.com>
2020-03-04 14:37:29 +0000

    Char stream works and but byte stream gets an EOFException

--- src/Y1S2/lab1/RandomNumberWriter.java
+++ src/Y1S2/lab1/RandomNumberWriter.java
@@ -1,5 +1,6 @@
 package Y1S2.lab1;
 
+import java.io.*;
 import java.util.Random;
 
 import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io.RandomIO;
@@ -12,12 +13,33 @@ public class RandomNumberWriter implements RandomIO {
     }
 
     public void writeRandomByte(String filename) {
+        try {
+            FileOutputStream out = new FileOutputStream(filename);
 
+            for (int i = 0; i < 10000; i++) {
+                String str = String.valueOf(random.nextInt(100000));
+                byte b[] = str.getBytes();
+                out.write(b);
+            }
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void writeRandomChars(String filename) {
+        try {
+            Writer writer = new FileWriter(filename);
 
+            for (int i = 0; i < 10000; i++) {
+                writer.write(String.valueOf(random.nextInt(100000)));
+                writer.write('\n');
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
