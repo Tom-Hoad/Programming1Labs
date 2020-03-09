a5fa9ec7b4a6d79978cf2a45eb8d8ea631e2224d
Tom-Hoad <tom.hoad03@gmail.com>
2020-03-09 21:49:50 +0000

    Attempting to recursively determine if KPalindrome

--- src/Y1S2/lab2/KPalindrome.java
+++ src/Y1S2/lab2/KPalindrome.java
@@ -4,10 +4,10 @@ import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.recursion.PalindromeCh
 
 public class KPalindrome implements PalindromeChecker {
     public boolean isKPalindrome(String s, int i) {
-        if (isPalindrome(s)) {
-            return true;
+        if (i == 1) {
+            return isPalindrome(s);
         } else {
-            return isKPalindrome(s, i);
+            return breakString(s, i - 1, 0);
         }
     }
 
@@ -16,15 +16,23 @@ public class KPalindrome implements PalindromeChecker {
         int midPoint = s.length() / 2;
 
         if (s.length() % 2 == 0) {
-            if (s.substring(0, midPoint - 1).equals(s.substring(midPoint))) {
-                return true;
-            }
+            return s.substring(0, midPoint - 1).equals(s.substring(midPoint));
         } else {
             midPoint = (int) Math.floor(midPoint);
-            if (s.substring(0, midPoint - 1).equals(s.substring(midPoint + 1))) {
-                return true;
-            }
+            return s.substring(0, midPoint - 1).equals(s.substring(midPoint + 1));
         }
-        return false;
+    }
+
+    // Breaks the string apart.
+    public boolean breakString(String s, int i, int count) {
+        if (count != s.length()) {
+            if (count == 0) {
+                return isKPalindrome(s.substring(0, 0) + s.substring(count + 1), i - 1);
+            } else {
+                return isKPalindrome(s.substring(0, count - 1) + s.substring(count + 1), i - 1);
+            }
+        } else {
+            return false;
+        }
     }
 }
