4196e616712d939951042498afdeb5fa1fcdbfa0
tom-hoad <tdh1g19@soton.ac.uk>
2020-05-13 11:35:46 +0100

    Improved my tests to work with the possible exceptions.

--- src/Y1S2/lab4/DAGSortTest.java
+++ src/Y1S2/lab4/DAGSortTest.java
@@ -2,15 +2,23 @@ package Y1S2.lab4;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class DAGSortTest {
+class DAGSortTest {
     @Test
-    public void nullParameter() {
-        try {
-            assertNull(DAGSort.sortDAG(null));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    void nullParameter() {
+        assertThrows(NullPointerException.class, () -> DAGSort.sortDAG(null), "Edges cannot be null.");
+    }
+
+    @Test
+    void inputCycles() {
+        int[][] edges = {{1}, {2}, {0}};
+        assertThrows(CycleDetectedException.class, () -> DAGSort.sortDAG(edges), "A cycle was found in the input graph.");
+    }
+
+    @Test
+    void invalidEdges() {
+        int[][] edges = {{1,2,5}, {-1,1,3}};
+        assertThrows(InvalidNodeException.class, () -> DAGSort.sortDAG(edges), "Edges must be labelled from 0 to edges.length - 1.");
     }
 }
