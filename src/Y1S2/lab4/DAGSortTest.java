675b0a8fc4de30a31a94371eea716670f85c979b
tom-hoad <tdh1g19@soton.ac.uk>
2020-05-15 15:03:49 +0100

    Testing if a correct topological order can be found.

--- src/Y1S2/lab4/DAGSortTest.java
+++ src/Y1S2/lab4/DAGSortTest.java
@@ -2,9 +2,33 @@ package Y1S2.lab4;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class DAGSortTest {
+    @Test
+    void correctOrder() throws CycleDetectedException, InvalidNodeException {
+        int[][] edges = {{2, 3}, {3, 4}, {5}, {}, {}, {4}};
+        assertTrue(checkOrders(edges), "Not found the correct topological order.");
+    }
+
+    // Checks if the given order matches any of the possible orders.
+    private boolean checkOrders(int[][] edges) throws CycleDetectedException, InvalidNodeException {
+        int[][] possibleOrders = {{0, 1, 2, 3, 5, 4}, {0, 1, 2, 5, 3, 4}, {0, 1, 2, 5, 4, 3}, {0, 1, 3, 2, 5, 4},
+                                    {1, 0, 2, 3, 5, 4}, {1, 0, 2, 5, 3, 4}, {1, 0, 2, 5, 4, 3}, {1, 0, 3, 2, 5, 4}};
+        int[] givenOrder = DAGSort.sortDAG(edges);
+        boolean found = false;
+
+        for (int[] possibleOrder : possibleOrders) {
+            if (Arrays.equals(givenOrder, possibleOrder)) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
     @Test
     void nullParameter() {
         assertThrows(NullPointerException.class, () -> DAGSort.sortDAG(null), "Edges cannot be null.");
